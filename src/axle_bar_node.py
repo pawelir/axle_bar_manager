0416dc81fadd9052b5eeea403ea0f20c272f1e8e
Paweł Irzyk <pawel.irzyk6@gmail.com>
2022-04-13 23:47:58 +0200

    Launch file added

--- src/axle_bar_node.py
+++ src/axle_bar_node.py
@@ -23,7 +23,7 @@ class AxleBarManager:
         self.axle_position = Float64()
         self.front_axle_state = JointControllerState()
         self.rear_axle_state = JointControllerState()
-        rospy.sleep(1)
+        rospy.sleep(0.5)
 
     def update_front_axle_state(self,msg) -> None:
         self.front_axle_state = msg
@@ -50,7 +50,7 @@ if __name__=="__main__":
     rospy.init_node('axle_bar_manager')
     try:
         axle_bar_manager = AxleBarManager()
-        rospy.loginfo('Node axle_bar_manager initialized!')
+        rospy.loginfo('[axle_bar_manager] Node initialized!')
     except rospy.ROSInterruptException:
         pass
     rospy.spin()
