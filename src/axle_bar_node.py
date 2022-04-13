8cf3af92eeaf58d675c87f0e79704f13f016f7fd
Paweł Irzyk <pawel.irzyk6@gmail.com>
2022-04-13 23:34:33 +0200

    Initial commit

---
+++ src/axle_bar_node.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import rospy
+from control_msgs.msg import JointControllerState
+from std_msgs.msg import Float64
+
+'''Module providing functions to control the Rotrac's axle bars'''
+
+class AxleBarManager:
+    def __init__(self):
+        self.front_axle_position_pub = rospy.Publisher('/rotrac_e2/front_axle_position_controller/command',
+                                                       Float64,
+                                                       queue_size=1)
+        self.rear_axle_position_pub = rospy.Publisher('/rotrac_e2/rear_axle_position_controller/command',
+                                                       Float64,
+                                                       queue_size=1)
+        self.front_axle_state_sub = rospy.Subscriber('/rotrac_e2/front_axle_position_controller/state', 
+                                                    JointControllerState,
+                                                    self.update_front_axle_state)
+        self.rear_axle_state_sub = rospy.Subscriber('/rotrac_e2/rear_axle_position_controller/state', 
+                                                    JointControllerState,
+                                                    self.update_rear_axle_state)
+        self.axle_position = Float64()
+        self.front_axle_state = JointControllerState()
+        self.rear_axle_state = JointControllerState()
+        rospy.sleep(1)
+
+    def update_front_axle_state(self,msg) -> None:
+        self.front_axle_state = msg
+    
+    def update_rear_axle_state(self,msg) -> None:
+        self.rear_axle_state = msg
+    
+    def axles_up (self) -> None:
+        self.axle_position.data = 0.1
+        self.front_axle_position_pub.publish(self.axle_position)
+        self.rear_axle_position_pub.publish(self.axle_position)
+
+    def axles_down (self) -> None:
+        self.axle_position.data = 0.0
+        self.front_axle_position_pub.publish(self.axle_position)
+        self.rear_axle_position_pub.publish(self.axle_position)
+
+    @property
+    def lowered(self) -> bool:
+        return (self.front_axle_state.process_value < 0.01 and
+                self.rear_axle_state.process_value < 0.01)
+
+if __name__=="__main__":
+    rospy.init_node('axle_bar_manager')
+    try:
+        axle_bar_manager = AxleBarManager()
+        rospy.loginfo('Node axle_bar_manager initialized!')
+    except rospy.ROSInterruptException:
+        pass
+    rospy.spin()
