2a2cca5d9fcf5ebece94d1457c9454cecc0049d9
Paweł Irzyk <pawel.irzyk6@gmail.com>
2022-04-15 17:04:16 +0200

    private poles

--- src/axle_bar_node.py
+++ src/axle_bar_node.py
@@ -17,28 +17,28 @@ class AxleBarManager:
                                                        queue_size=1)
         self.sub_front_axle_state = rospy.Subscriber('/rotrac_e2/front_axle_position_controller/state', 
                                                     JointControllerState,
-                                                    self.update_front_axle_state)
+                                                    self._update_front_axle_state)
         self.sub_rear_axle_state = rospy.Subscriber('/rotrac_e2/rear_axle_position_controller/state', 
                                                     JointControllerState,
-                                                    self.update_rear_axle_state)
+                                                    self._update_rear_axle_state)
         self.srv_raise_bars = rospy.Service("axle_bar_manager/raise_axle_bars",
                                             Trigger,
-                                            self.raise_axle_bars_cb)
+                                            self._raise_axle_bars_cb)
         self.srv_raise_bars = rospy.Service("axle_bar_manager/lower_axle_bars",
                                             Trigger,
-                                            self.lower_axle_bars_cb)
-        self.front_axle_state = JointControllerState()
-        self.rear_axle_state = JointControllerState()
-        while not self.front_axle_state or not self.rear_axle_state:
+                                            self._lower_axle_bars_cb)
+        self._front_axle_state = JointControllerState()
+        self._rear_axle_state = JointControllerState()
+        while not self._front_axle_state or not self._rear_axle_state:
             rospy.loginfo("Waiting for axle position controller")
 
-    def update_front_axle_state(self, msg: JointControllerState) -> None:
-        self.front_axle_state = msg
+    def _update_front_axle_state(self, msg: JointControllerState) -> None:
+        self._front_axle_state = msg
     
-    def update_rear_axle_state(self, msg: JointControllerState) -> None:
-        self.rear_axle_state = msg
+    def _update_rear_axle_state(self, msg: JointControllerState) -> None:
+        self._rear_axle_state = msg
     
-    def raise_axle_bars_cb (self) -> None:
+    def _raise_axle_bars_cb (self) -> None:
         rospy.logdebug("Raise axle bars service called")
         axle_position = Float64(data=0.1)
         self.pub_front_axle_position.publish(axle_position)
@@ -50,7 +50,7 @@ class AxleBarManager:
         rospy.logdebug("Raise axle bars service finished")
         return TriggerResponse(success=self.raised)
 
-    def lower_axle_bars_cb (self) -> None:
+    def _lower_axle_bars_cb (self) -> None:
         rospy.logdebug("Lower axle bars service called")
         axle_position = Float64(data=0.0)
         self.pub_front_axle_position.publish(axle_position)
@@ -68,13 +68,13 @@ class AxleBarManager:
 
     @property
     def lowered(self) -> bool:
-        return (self.front_axle_state.process_value < 0.01 and
-                self.rear_axle_state.process_value < 0.01)
+        return (self._front_axle_state.process_value < 0.01 and
+                self._rear_axle_state.process_value < 0.01)
     
     @property
     def raised(self) -> bool:
-        return (self.front_axle_state.process_value > 0.09 and
-                self.rear_axle_state.process_value > 0.09)
+        return (self._front_axle_state.process_value > 0.09 and
+                self._rear_axle_state.process_value > 0.09)
 
 if __name__=="__main__":
     rospy.init_node('axle_bar_manager')
